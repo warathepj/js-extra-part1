f73886a8f8a3e9877f5093dfa7465dc93813ebd5
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-17 11:29:54 +0700

    13 48

--- extract.js
+++ extract.js
@@ -1,12 +1,20 @@
 // Extract Object
-const name = 'Jiant';
-const pet = {
-    name: 'Hakao',
-    breed: 'Shizu',
-    color: 'White'
-};
+// const name = 'Jiant';
+// const pet = {
+//     name: 'Hakao',
+//     breed: 'Shizu',
+//     color: 'White'
+// };
 
-const { name: rName, ...others } = pet;
+// const { name: rName, ...others } = pet;
 
-console.log(rName);
-console.log(others);
+// console.log(rName);
+// console.log(others);
+
+const scores = [92, 86, 72, 60, 51];
+
+const [firstScore, secondScore, ...otherScores] = scores;
+
+console.log(firstScore);
+console.log(otherScores);
+
